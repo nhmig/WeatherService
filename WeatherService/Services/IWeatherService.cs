e64e380150068cd978d166cec3067e37fd75a591
Bashkirov Nikolay <javahello@mail.ru>
2021-06-05 15:32:44 +0300

    Works

--- WeatherService/Services/IWeatherService.cs
+++ WeatherService/Services/IWeatherService.cs
@@ -6,7 +6,6 @@ namespace WeatherService.Service
 {
     public interface IWeatherService
     {
-        string DegreeToDirection(int deg);
         Task<List<ResponseForecast>> GetForecast5(string cityName, string units);
         Task<ResponseTemperature> GetTemperature(string cityName, string units);
         Task<ResponseWind> GetWind(string cityName);
